fdaa68193c90ac5656a4547f1290f40bac88a030
karishmargaj <karishmargaj@gmai.com>
2023-07-18 14:47:24 +0530

    added vaccination center model class

---
+++ SpringBootHibernateIntegration/src/main/java/com/cognizant/model/VaccinationCenter.java
@@ -0,0 +1,26 @@
+package com.cognizant.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VaccinationCenter {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String vaccinationCenterName;
+	
+	private String vaccinationCenterAddress;
+	
+	
+}
